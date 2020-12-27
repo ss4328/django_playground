daf8ddbd0c7c4fc118e0d639d59d42dc662bfc0f
Shiv <ss4328@drexel.edu>
2020-12-27 16:42:52 -0500

    render data from db via model

--- playground_django/urls.py
+++ playground_django/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from pages.views import home_view, contact_view, social_view, about_view
-
+from products.views import product_detail_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,5 +25,6 @@ urlpatterns = [
     path('contact/', contact_view),
     path('about/', about_view),
     path('social/', social_view),
+    path('product/', product_detail_view)
 
 ]
